d794a673c61c72356908f931be2a15dd7cc2303b
ColdZerk <124044635+ColdZerk@users.noreply.github.com>
2023-02-26 15:26:42 +0300

    Update python counter.py

--- python counter.py
+++ python counter.py
@@ -1,4 +1,4 @@
-for i in range(1,11):
-    print("I like coding in " + i + " secs")
-    for y in range(1,4):
-        print("Current var Y is " + y)
+for jjj in range(1,11):
+    print("I like coding in " + jjj + " secs")
+    for ggg in range(1,4):
+        print("Current var ggg is " + ggg)
